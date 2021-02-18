2abab19718e376ca239c20735b356225dcf9903b
nitishsadire <nitishsandhu9@gmail.com>
2021-02-18 19:38:40 +0530

    merging multiple commits

--- Data/Frames.py
+++ Data/Frames.py
@@ -5,18 +5,28 @@ class Frames:
     def __init__(self, video_path, destination_path):
         self.video_path = video_path
         self.destination_path = destination_path + self.video_path.split("/")[-1].split(".")[0]
-        os.mkdir(self.destination_path)
+        self.minimumNumberOfFrames = 30
+        if os.path.isdir(self.destination_path) == False:
+            os.mkdir(self.destination_path)
+        self.isHavingMinimumNumberOfFrames = True
 
     def get_frames_path(self):
         return self.destination_path+"/"
 
+    def doesSatisfiesMinimumFramesConstraint(self):
+        return self.isHavingMinimumNumberOfFrames
+
     def generate_frames(self):
         video = cv2.VideoCapture(self.video_path)
         success_flag, frame = video.read()
-        numberOfFrames = 1
-
-        while success_flag:
-            cv2.imwrite(self.destination_path + '/frame{:06d}.jpg'.format(numberOfFrames), frame)
-            success_flag, frame = video.read()
-            numberOfFrames += 1
+        numberOfFrames = self.minimumNumberOfFrames + 1
+        if len(os.listdir(self.destination_path)) == 0:
+            numberOfFrames = 0
+            while success_flag and numberOfFrames < self.minimumNumberOfFrames:
+                cv2.imwrite(self.destination_path + '/frame{:06d}.jpg'.format(numberOfFrames), frame)
+                success_flag, frame = video.read()
+                numberOfFrames += 1
+        if numberOfFrames < self.minimumNumberOfFrames:
+            self.isHavingMinimumNumberOfFrames = False
+            os.system("rm -r " + self.destination_path)
 
--- Data/VideoDataset.py
+++ Data/VideoDataset.py
@@ -4,7 +4,8 @@ from torch.utils.data.dataset import Dataset
 from torchvision import transforms
 import os
 from PIL import Image
-import re
+import torch
+from sklearn import preprocessing
 
 class VideoDataset(Dataset):
     def __init__(self, video_path, destination_path, extensions):
@@ -15,34 +16,56 @@ class VideoDataset(Dataset):
         self.videoFilesPaths = videoFiles.get_files_paths()
         self.n = len(self.videoFilesPaths)
         self.frames=[]
+        self.le = preprocessing.LabelEncoder()
+        self.lessFramesThanThresholdList = []
+
+        # UCF-11 dataset
+        # self.le.fit(["basketball","biking","diving","golf_swing","horse_riding","soccer_juggling","swing","tennis_swing","trampoline_jumping","volleyball_spiking","walking"])
+        # UCF-50 dataset
+        self.le.fit(["BaseballPitch", "Basketball", "BenchPress", "Biking", "Billiards", "BreastStroke", "CleanAndJerk", "Diving", "Drumming", "Fencing", "GolfSwing", "HighJump", "HorseRace", "HorseRiding", "HulaHoop", "JavelinThrow", "JugglingBalls", "JumpingJack", "JumpRope", "Kayaking", "Lunges", "MilitaryParade", "Mixing", "Nunchucks", "PizzaTossing", "PlayingGuitar", "PlayingPiano", "PlayingTabla", "PlayingViolin", "PoleVault", "PommelHorse", "PullUps", "Punch", "PushUps", "RockClimbingIndoor", "RopeClimbing", "Rowing", "SalsaSpin", "SkateBoarding", "Skiing", "Skijet", "SoccerJuggling", "Swing", "TaiChi", "TennisSwing", "ThrowDiscus", "TrampolineJumping", "VolleyballSpiking", "WalkingWithDog", "YoYo"])
+
+
         for idx, video in enumerate(self.videoFilesPaths):
+            print("generating frames for video #", idx, ", ", video)
             self.frames.append(Frames(video, self.destination_path))
-            self.frames[-1].generate_frames()
+            self.frames[idx].generate_frames()
+            if self.frames[idx].doesSatisfiesMinimumFramesConstraint() == False:
+                self.lessFramesThanThresholdList.append(idx)
+        self.removeLessFramesThanThresholdVideos()
 
-    def get_label(self, frame_path):
+    def removeLessFramesThanThresholdVideos(self):
+        for idx in sorted(self.lessFramesThanThresholdList, reverse=True):
+            del self.frames[idx]
+            del self.videoFilesPaths[idx]
+        self.n = len(self.videoFilesPaths)
+
+    def get_label(self, videoFilePath):
         '''Assuming video has label name in its name'''
-        m = re.search("v_(.*)_g", frame_path)
-        return m.group(1)
+        # print(str(videoFilePath), flush=True)
+        # "hi"
+        return self.le.transform(list(str(videoFilePath.split("/")[-2]).split(" ")))
 
+    # ! ls - l
+    # UCF11_updated_mpg / basketball / v_shooting_01 / v_shooting_01_01.mpg
     def get_images(self, images_path):
         images = []
-        transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Resize((220, 220))])
+        transform = transforms.Compose([transforms.Resize((224, 224)),
+                                        transforms.ToTensor(),
+                                        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                                        ])
 
         for path, subdirs, files in os.walk(images_path):
-            for name in files:
-                image = Image.open(path + name).convert('RGB')
+            for i in range(0,30):
+                image = Image.open(path + 'frame{:06d}.jpg'.format(i))
                 image = transform(image)
                 images.append((image))
-        return images
+        return  torch.stack(images, dim=0)
 
     def __getitem__(self, index):
         frames_path = self.frames[index].get_frames_path()
-        images, label = self.get_images(frames_path), self.get_label(frames_path)
-        return (images, label)
+        # print(self.videoFilesPaths[index], flush=True)
+        images, label = self.get_images(frames_path), self.get_label(self.videoFilesPaths[index])
+        return images,label
 
     def __len__(self):
         return len(self.videoFilesPaths)
-
-
